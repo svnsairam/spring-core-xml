1ba7db80e0931d1ebf59554a2eabd0b0129be1a5
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-01-16 05:23:29 +0530

    Task 3 Initial Commit Used the Autowiring of the properties and Used Application Context to create Beans

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -16,21 +16,8 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1 = (Movie) context.getBean("movieBB2");
-        System.out.println("Actor Name using Application Context = " + movie1.getActor().getName());
-        System.out.println("Actor Gender using Application Context = " + movie1.getActor().getGender());
-        System.out.println("Actor Age using Application Context = " + movie1.getActor().getAge());
-
-        Movie movie2 = (Movie) context.getBean("movieBB2");
-        System.out.println(movie1 == movie2);
-
-        Movie movie3 = (Movie) context.getBean("movieKhaleja");
-        System.out.println("Actor Name using Application Context = " + movie3.getActor().getName());
-        System.out.println("Actor Gender using Application Context = " + movie3.getActor().getGender());
-        System.out.println("Actor Age using Application Context = " + movie3.getActor().getAge());
-
-        Movie movie4 = (Movie) context.getBean("movieSVSC");
-        System.out.println("Actor Name using Application Context = " + movie4.getActor().getName());
-        System.out.println("Actor Gender using Application Context = " + movie4.getActor().getGender());
-        System.out.println("Actor Age using Application Context = " + movie4.getActor().getAge());
+        System.out.println("Actor Name using Application Context and AutoWiring= " + movie1.getActor().getName());
+        System.out.println("Actor Gender using Application Context and AutoWiring = " + movie1.getActor().getGender());
+        System.out.println("Actor Age using Application Context and AutoWiring = " + movie1.getActor().getAge());
     }
 }
