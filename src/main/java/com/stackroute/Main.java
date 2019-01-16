bd9be3a7411305644747b559877b056862d897c7
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-01-16 06:14:22 +0530

    Task 4 Initial Commit. We used the Aware Interfaces to pre-set the properties of the bean before creation of the bean and Used Application Context to create Beans

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -15,9 +15,20 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1 = (Movie) context.getBean("movieBB2");
+        Movie movie1 = (Movie) context.getBean("movieSVSC");
+        // Even though the ref="actorMahesh" we get the Prabhas,male,38  because we have pre-setted the actor properties
+        // (this.actor) before a movie bean is created
+        // of movie bean using setApplicationContext & setBeanFactory methods which are present in Movie Class.
         System.out.println("Actor Name using Application Context and AutoWiring= " + movie1.getActor().getName());
         System.out.println("Actor Gender using Application Context and AutoWiring = " + movie1.getActor().getGender());
         System.out.println("Actor Age using Application Context and AutoWiring = " + movie1.getActor().getAge());
+
+        Movie movie2 = (Movie) context.getBean("movieRRR");
+        // Since the of the Movie bean wiht id="movieRRR" has scope = "prototype" it is not pre-setted its properties
+        // But when we try to create bean it will again pre-set the properties (this.actor) using the
+        // setApplicationContext & setBeanFactory methods which are present in Movie Class.
+        System.out.println("Actor Name using Application Context and AutoWiring= " + movie2.getActor().getName());
+        System.out.println("Actor Gender using Application Context and AutoWiring = " + movie2.getActor().getGender());
+        System.out.println("Actor Age using Application Context and AutoWiring = " + movie2.getActor().getAge());
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     private Actor actor;
 
     public Movie() {
@@ -17,5 +24,26 @@ public class Movie {
 
     public Actor getActor() {
         return this.actor;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        // Used to set the properties of the bean before its initalized
+        this.actor = (Actor) beanFactory.getBean("actorPrabhas");
+        System.out.println("From BeanFactory Aware = "+actor.getName());
+        System.out.println("From BeanFactory Aware = "+actor.getGender());
+        System.out.println("From BeanFactory Aware = "+actor.getAge());
+    }
+
+    public void setBeanName(String s) {
+        // Gets the bean names of all Movie beans and scope != prototype
+        System.out.println("From BeanName Aware the name is =  "+s);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        // Used to set the properties of the bean before its initalized
+        this.actor = (Actor) applicationContext.getBean("actorPrabhas");
+        System.out.println("From Application Context Aware = "+actor.getName());
+        System.out.println("From Application Context Aware = "+actor.getGender());
+        System.out.println("From Application Context Aware = "+actor.getAge());
     }
 }
